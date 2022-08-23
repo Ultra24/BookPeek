a33e05ca640bdb206d81f5a9dd8eb993050f6409
umer <62357227+Ultra24@users.noreply.github.com>
2022-08-23 17:08:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-/* 10-08-22
-   Written on Umer's Macbook Pro
+/* 
+10-08-22
+Written on Umer's Macbook Pro
 */
 
 document.querySelector("#add").onclick = function addBooksRead() {
+     let booksRead = document.getElementById("books-read").value;
+    let booksReadID = document.getElementById("books-read");
+    let booksReadList = document.getElementById("booksReadList");
 }
